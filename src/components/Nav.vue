ebc0087d09506aa3b7c5e264f91030580185f529
Jasper <maple0517@gmail.com>
2018-06-11 09:42:08 +0800

    first

---
+++ src/components/Nav.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li>{{msg}}</li>
+            <li>{{msg}}</li>
+            <li>{{msg}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'nav',
+    data () {
+        return {
+        msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .nav{
+        width: 600px;
+        height: 100px;
+        background-color: RGBA(100, 146, 214, 1.00);
+    }
+</style>
+
